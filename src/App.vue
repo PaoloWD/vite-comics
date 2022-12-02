9dac5763b59a1aa23be613699a4ad028339524eb
PaoloWD <paolo.lore.93@gmail.com>
2022-12-02 15:18:53 +0100

    Add navbar

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div><NavbarVue></NavbarVue></div>
+  </div>
+</template>
+<script>
+import NavbarVue from "./components/Navbar.vue";
+import TheFooterVue from "./components/TheFooter.vue";
+import TheMainVue from "./components/TheMainBanner.vue";
+export default {
+  components: { NavbarVue, TheFooterVue, TheMainVue },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="d-flex justify-content-around align-items-center pt-3">
+    <nav>
+      <img src="../../public/img/favicon.ico" alt="" />
+    </nav>
+    <div class="d-flex justify-content-between gap-3">
+      <div v-for="item in items">
+        <a href="">{{ item }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        "CHARACTERS",
+        "COMICS",
+        "MOVIES",
+        "TV",
+        "GAMES",
+        "COLLECTIBLES",
+        "VIDEOS",
+        "FANS",
+        "NEWS",
+        "SHOP",
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
+<style scoped>
+img {
+  width: 40px;
+}
+a {
+  font-size: 0.7rem;
+  text-decoration: none;
+}
+</style>
